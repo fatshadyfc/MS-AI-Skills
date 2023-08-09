c414fdd75f52321831dfc1ca93ba2c948f9254e5
fatshadyfc <141872423+fatshadyfc@users.noreply.github.com>
2023-08-09 21:35:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,4 @@
+function skillsMember() {
+    var skills = document.getElementById('skills');
+    var skillsMember = document.getElementById('skillsMember');
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedInd
